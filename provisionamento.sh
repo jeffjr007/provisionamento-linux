c770699fbae634c4fba3d735164d3984d0610e69
Jeferson Junior <jeffjr007z@gmail.com>
2025-04-18 16:44:01 -0300

    Add files via upload

---
+++ provisionamento.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+echo "Removendo usuários, grupos e diretórios antigos..."
+
+for user in carlos maria joao debora sebastiana roberto josefina amanda rogerio; do
+    userdel -r $user 2>/dev/null
+done
+
+for group in GRP_ADM GRP_VEN GRP_SEC; do
+    groupdel $group 2>/dev/null
+done
+
+rm -rf /publico /adm /ven /sec
+
+echo "Criando diretórios..."
+
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
+
+echo "Criando grupos..."
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo "Criando usuários e adicionando aos grupos..."
+
+useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+
+useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+
+useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+
+echo "Aplicando permissões dos diretórios..."
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /publico
+
+echo "Finalizado com sucesso!"
